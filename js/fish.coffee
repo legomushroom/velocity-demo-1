8534ab7c73534436dba60b6b4ec2740549eda4fa
sol0mka <legomushroom@gmail.com>
2014-07-18 18:52:20 +0300

    add bottom splash

--- js/fish.coffee
+++ js/fish.coffee
@@ -11,13 +11,14 @@ class Fish
     @delay = @o.delay or 0
     @s = 1*h.time 1
 
-    @$bottomSplash = $('#js-bottom-splash')
+    @$bottomSplash  = $('#js-bottom-splash')
+    @$bottomSplash2 = $('#js-bottom-splash2')
 
     @$fish  = $('#js-fish')
     @$fish.css 'transform-origin': 'center center'
     @$fish.velocity {
       translateX: 35
-      translateY: 300
+      translateY: 390
       }, duration: 1
 
     @$fishW = $('#js-fish-wrapper')
@@ -27,40 +28,69 @@ class Fish
     startX = parseInt position[1], 10
     startY = parseInt position[2], 10
 
-
-    fadeRadius = 10
+    bubbleRadius = 20
+    delayForSplash = 100
     @$bottomSplash.children().each (i,item)=>
       $item = $ item
-      $item.velocity {
-        translateX: h.rand(-fadeRadius,fadeRadius)
-        translateY: h.rand(-fadeRadius,fadeRadius)
-        r: h.rand(10, 15)
-        },
-          duration: 200*@s
-
-      if i > 5 and i < 10
+      if i > 5 and i < 12
         $item.velocity {
-          translateX: h.rand(-10*fadeRadius,10*fadeRadius)
-          translateY: h.rand(-10*fadeRadius,10*fadeRadius)
-          r: 0
-          rotateZ: h.rand(-180,180)
+          translateX: h.rand(-bubbleRadius,bubbleRadius)
+          translateY: h.rand(0,bubbleRadius)
+          r: h.rand(5, 20)
           },
             duration: 400*@s
+
+        $item.velocity {
+          translateX: h.rand(-5*bubbleRadius,5*bubbleRadius)
+          translateY: h.rand(-5*bubbleRadius,5*bubbleRadius)
+          r: 0
+          },
+            duration: 700*@s
+            delay:    h.rand(50,100)*@s
       else
+        $item.velocity {
+          translateX: h.rand(-bubbleRadius,bubbleRadius)
+          translateY: h.rand(0,bubbleRadius)
+          r: h.rand(15, 20)
+          },
+            duration: 300*@s + h.rand(50,100)*@s
+            delay: h.rand(50,150)*@s + delayForSplash*@s
+
         $item.velocity {
           translateX: 0
           translateY: 0
           r: 0
+          rotateZ: h.rand(-100,100)
           },
-            duration: 400*@s
+            duration: 800*@s
+
+
+    @$bottomSplash2.children().each (i,item)=>
+      $item = $ item
+      $item.velocity {
+        translateX: h.rand(-bubbleRadius,bubbleRadius)
+        translateY: h.rand(-bubbleRadius,0)
+        r: h.rand(10, 15)
+        },
+          duration: 300*@s
+          delay: h.rand(50,150)*@s + delayForSplash*@s
+
+      $item.velocity {
+        translateX: 0
+        translateY: 0
+        r: 0
+        rotateZ: h.rand(-100,100)
+        },
+          duration: 800*@s
 
 
     @$fish.velocity {
       translateX: 30
       translateY: -150
       },
-        duration: 1200*@s
-        easing: 'ease-in-out'
+        duration: 2000*@s
+        easing: 'linear'
+        delay:  100*@s + delayForSplash*@s
 
     # @$fishW.velocity {
     #   opacity: 100
--- js/fish.js
+++ js/fish.js
@@ -21,13 +21,14 @@
       this.delay = this.o.delay || 0;
       this.s = 1 * h.time(1);
       this.$bottomSplash = $('#js-bottom-splash');
+      this.$bottomSplash2 = $('#js-bottom-splash2');
       this.$fish = $('#js-fish');
       this.$fish.css({
         'transform-origin': 'center center'
       });
       this.$fish.velocity({
         translateX: 35,
-        translateY: 300
+        translateY: 390
       }, {
         duration: 1
       });
@@ -35,48 +36,81 @@
     };
 
     Fish.prototype.run = function() {
-      var fadeRadius, position, startX, startY;
+      var bubbleRadius, delayForSplash, position, startX, startY;
       position = this.$fishW.attr('transform').split(/translate\(|,|\)/);
       startX = parseInt(position[1], 10);
       startY = parseInt(position[2], 10);
-      fadeRadius = 10;
+      bubbleRadius = 20;
+      delayForSplash = 100;
       this.$bottomSplash.children().each((function(_this) {
         return function(i, item) {
           var $item;
           $item = $(item);
-          $item.velocity({
-            translateX: h.rand(-fadeRadius, fadeRadius),
-            translateY: h.rand(-fadeRadius, fadeRadius),
-            r: h.rand(10, 15)
-          }, {
-            duration: 200 * _this.s
-          });
-          if (i > 5 && i < 10) {
-            return $item.velocity({
-              translateX: h.rand(-10 * fadeRadius, 10 * fadeRadius),
-              translateY: h.rand(-10 * fadeRadius, 10 * fadeRadius),
-              r: 0,
-              rotateZ: h.rand(-180, 180)
+          if (i > 5 && i < 12) {
+            $item.velocity({
+              translateX: h.rand(-bubbleRadius, bubbleRadius),
+              translateY: h.rand(0, bubbleRadius),
+              r: h.rand(5, 20)
             }, {
               duration: 400 * _this.s
             });
+            return $item.velocity({
+              translateX: h.rand(-5 * bubbleRadius, 5 * bubbleRadius),
+              translateY: h.rand(-5 * bubbleRadius, 5 * bubbleRadius),
+              r: 0
+            }, {
+              duration: 700 * _this.s,
+              delay: h.rand(50, 100) * _this.s
+            });
           } else {
+            $item.velocity({
+              translateX: h.rand(-bubbleRadius, bubbleRadius),
+              translateY: h.rand(0, bubbleRadius),
+              r: h.rand(15, 20)
+            }, {
+              duration: 300 * _this.s + h.rand(50, 100) * _this.s,
+              delay: h.rand(50, 150) * _this.s + delayForSplash * _this.s
+            });
             return $item.velocity({
               translateX: 0,
               translateY: 0,
-              r: 0
+              r: 0,
+              rotateZ: h.rand(-100, 100)
             }, {
-              duration: 400 * _this.s
+              duration: 800 * _this.s
             });
           }
+        };
+      })(this));
+      this.$bottomSplash2.children().each((function(_this) {
+        return function(i, item) {
+          var $item;
+          $item = $(item);
+          $item.velocity({
+            translateX: h.rand(-bubbleRadius, bubbleRadius),
+            translateY: h.rand(-bubbleRadius, 0),
+            r: h.rand(10, 15)
+          }, {
+            duration: 300 * _this.s,
+            delay: h.rand(50, 150) * _this.s + delayForSplash * _this.s
+          });
+          return $item.velocity({
+            translateX: 0,
+            translateY: 0,
+            r: 0,
+            rotateZ: h.rand(-100, 100)
+          }, {
+            duration: 800 * _this.s
+          });
         };
       })(this));
       return this.$fish.velocity({
         translateX: 30,
         translateY: -150
       }, {
-        duration: 1200 * this.s,
-        easing: 'ease-in-out'
+        duration: 2000 * this.s,
+        easing: 'linear',
+        delay: 100 * this.s + delayForSplash * this.s
       });
     };
 
