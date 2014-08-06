776110a84593983e5ce62650b8796a38c7e5d23f
sol0mka <legomushroom@gmail.com>
2014-08-06 12:35:15 +0300

    amend

--- js/main.js
+++ js/main.js
@@ -10,8 +10,32 @@
     }
 
     Main.prototype.run = function() {
-      return new Meets({
+      new Caleydoscope({
         delay: h.time(0)
+      });
+      new Meets({
+        delay: h.time(2900)
+      });
+      new Svg({
+        delay: h.time(4900)
+      });
+      new Triangles({
+        delay: h.time(7900)
+      });
+      new Tribits({
+        delay: h.time(8950)
+      });
+      new Waves({
+        delay: h.time(10300)
+      });
+      new Fish({
+        delay: h.time(11500)
+      });
+      new Logo({
+        delay: h.time(13300)
+      });
+      return new Mushroom({
+        delay: h.time(15300)
       });
     };
 
